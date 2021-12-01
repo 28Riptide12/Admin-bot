fe5d1faef050620037fbfc99af4db3140d89a1db
28Riptide12 <76173025+28Riptide12@users.noreply.github.com>
2021-12-01 18:36:00 +0000

    Update Info.ts

--- commands/Info.ts
+++ commands/Info.ts
@@ -13,7 +13,7 @@ export default {
     callback: ({ message, text }) => {
         const embed = new MessageEmbed()
         .setTitle('INFO')
-        .setDescription"Admin bot is a multifunction discord bot.\n\ngithub-https://github.com/28Riptide12/Admin-bot \n\nPlease credit me when you use this bot in your discord servers."
+        .setDescription"Admin bot is a multifunction discord bot.\n\ngithub-https://github.com/28Riptide12/Admin-bot \n\nPlease credit me i you decide to use this bot in your discord servers."
         return embed
     }
 } as ICommand    
